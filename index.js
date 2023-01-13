4611dabd9de4faa26ee73e66d62b753f7a7d3db8
Toxxxy <tonyxxxq@gmail.com>
2023-01-13 02:39:47 +0200

    fixed errors

--- index.js
+++ index.js
@@ -22,25 +22,45 @@ app.get('/', (req, res) => {
 });
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+            _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '30d',
+            },
+        );
+
+        res.json({
+            ...user,
+            token,
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не вдалося зареєструватись',
+        })
     }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-    const user = await doc.save();
-
-    res.json(user)
 });
 
 const PORT = 3000;
