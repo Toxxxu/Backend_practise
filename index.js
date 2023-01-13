229ae8fd1a194859aa13888c282b424e5f1184e8
Toxxxy <tonyxxxq@gmail.com>
2023-01-13 02:22:23 +0200

    added validator + register

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+
+import UserModel from './models/model.js';
 
 mongoose
-    .connect('mongodb+srv://admin:1234@cluster0.ubqswce.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://admin:1234@cluster0.ubqswce.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => { console.log('MongoDB connected'); })
     .catch((err) => { console.log('MongoDB error', err); });
 
@@ -15,8 +21,26 @@ app.get('/', (req, res) => {
     res.send("Hello");
 });
 
-app.post('/auth/register', (req, res) => {
-    
+app.post('/auth/register', registerValidation, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user)
 });
 
 const PORT = 3000;
